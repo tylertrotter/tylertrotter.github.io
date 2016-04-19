f65ee0f69ad12473d9562bcbb279e7105c16ce0d
Tyler Trotter <tylertrotter@gmail.com>
2016-04-18 22:00:37 -0400

    Sassified

---
+++ Gruntfile.js
@@ -0,0 +1,19 @@
+module.exports = function(grunt) {
+    
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        sass: { 
+            dist: { 
+                options: {  
+                    style: 'compressed'
+                },
+                files: {   
+                    'css/main.css': 'sass/main.scss'
+                }
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-contrib-sass');
+    grunt.registerTask('default', ['sass']);
+};
