11318395fbf5a1551d404c6fb9108eada686ce95
Tyler Trotter <tylertrotter@gmail.com>
2023-01-29 10:51:11 -0500

    Remove unnessary code

--- common.js
+++ common.js
@@ -8,14 +8,3 @@ function handleKeyboardUse() {
 function handleMouseUse() {
   document.documentElement.setAttribute('data-input', 'mouse');
 }
-
-if (window.location.search) {
-  const id = window.location.search.replace('?', '');
-  document.querySelectorAll('a').forEach(a => {
-    const href = a.getAttribute('href');
-    if (href && href.indexOf('https://') === -1 && href.indexOf('.pdf') === -1) {
-      const qsChar = (href.indexOf('?')) > -1 ? '&' : '?';
-      a.setAttribute('href', a.getAttribute('href') + qsChar + id);
-    }
-  })
-}
