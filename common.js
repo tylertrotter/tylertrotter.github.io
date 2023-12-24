4191ef62f543ed8f2e44f2af9aa97b0c46664430
Tyler Trotter <tylertrotter@gmail.com>
2023-12-24 10:13:04 -0500

    more changes

--- common.js
+++ common.js
@@ -8,3 +8,11 @@ function handleKeyboardUse() {
 function handleMouseUse() {
   document.documentElement.setAttribute('data-input', 'mouse');
 }
+
+// document.addEventListener("scroll", e => {
+//   document.documentElement.style.setProperty('--scroll-factor', document.documentElement.scrollTop / 5);
+//   const works = document.querySelectorAll('.work');
+//   for(let i = 0; i < works.length; i++) {
+//     works[i].style.setProperty('--scroll-factor', Math.max(0, (document.documentElement.scrollTop / 5) - (works[i].offsetTop / 8)));
+//   }
+// });
