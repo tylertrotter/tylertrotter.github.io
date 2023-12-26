c18a6631e85fab0f869462c31535c96024e7f2d2
Tyler Trotter <tylertrotter@gmail.com>
2023-12-25 20:45:17 -0500

    more work

--- common.js
+++ common.js
@@ -8,3 +8,11 @@ function handleKeyboardUse() {
 function handleMouseUse() {
   document.documentElement.setAttribute('data-input', 'mouse');
 }
+
+document.addEventListener('scroll', () => {
+  let scrollPercent = document.documentElement.scrollTop / (document.documentElement.scrollHeight - document.documentElement.clientHeight);
+  if (scrollPercent > .75)
+    document.documentElement.classList.add('scrolled');
+  else
+    document.documentElement.classList.remove('scrolled');
+});
